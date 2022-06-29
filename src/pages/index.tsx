95bed308f4ea617f4885b21d192718c2eaecede9
Patrick Xin <patrick.xin.dev@gmail.com>
2022-06-29 12:52:00 -0700

    ci: add eslint, prettier, husky

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <div className='flex min-h-screen flex-col items-center justify-center py-2'>
+      hello world
+    </div>
+  );
+};
+
+export default Home;
